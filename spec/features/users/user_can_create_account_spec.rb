ac373b352716cb17afba70b73046995726ebba6f
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-06 14:33:29 -0600

    initial feature test for account creation

---
+++ spec/features/users/user_can_create_account_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "User can create an account" do
+  scenario "if not already in database" do
+
+    visit '/'
+
+    click_on("Create New Account")
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in 'user[username]', with: 'Dude'
+    fill_in 'user[password]', with: 'password'
+
+    click_on("Create Account")
+
+    expect(current_path).to eq user_path(User.last)
+    expect(page).to have_content("Hi Dude")
+    expect(page).to have_content("Successful signup")
+  end
+end
