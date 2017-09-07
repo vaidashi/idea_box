7485bafdc6dded816584334002fdc300154ab64e
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-07 14:38:43 -0600

    admin authorization implemented

--- spec/features/users/user_can_view_list_of_categories_spec.rb
+++ spec/features/users/user_can_view_list_of_categories_spec.rb
@@ -4,11 +4,10 @@ require 'rails_helper'
 describe "User can view list of categories" do
   scenario "from categories index page" do
     user = create(:user)
-    # user1 = User.create(username: "Dude", password: "123")
 
     category1 = create(:category)
     category2 = create(:category)
-    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit '/categories'
 
@@ -16,5 +15,15 @@ describe "User can view list of categories" do
     expect(page).to have_content(category2.name)
     expect(page).to_not have_link "Edit"
     expect(page).to_not have_link "Delete"
+  end
+
+  scenario "but can't create a category" do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit new_admin_category_path
+
+    expect(page).to have_content("The page you were looking for doesn't exist.")
   end
 end
