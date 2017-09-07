bd45e2f8b0e6a7f8004d21639ff3b4086c3a4f86
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-07 14:51:28 -0600

    destroy category function added for admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -18,6 +18,14 @@ class Admin::CategoriesController < Admin::BaseController
     end
   end
 
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+    flash[:success] = "#{category.name} was successfully deleted!"
+
+    redirect_to admin_categories_path
+  end
+
   private
 
   def category_params
--- spec/features/admin/admin_can_create_and_destroy_a_category_spec.rb
+++ spec/features/admin/admin_can_create_and_destroy_a_category_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 feature "When a user visits the categories page" do
-
   context "as an admin" do
+
     scenario "they can see list of all categories" do
       admin = User.create(username: "admin", password: "boom", role: "admin")
       category1 = create(:category)
@@ -31,6 +31,24 @@ feature "When a user visits the categories page" do
       expect(current_path).to eq(admin_categories_path)
       expect(Category.count).to eq(1)
       expect(page).to have_content("Cleaning")
+    end
+
+    scenario "they can destroy a category" do
+      admin = User.create(username: "admin", password: "boom", role: "admin")
+      category1 = create(:category)
+      category2 = create(:category)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_categories_path
+      expect(Category.count).to eq(2)
+      expect(page).to have_content(category1.name)
+
+      click_on("Delete", :match => :first)
+
+      expect(current_path).to eq(admin_categories_path)
+      expect(Category.count).to eq(1)
+      expect(page).to have_content(category2.name)
     end
   end
 
