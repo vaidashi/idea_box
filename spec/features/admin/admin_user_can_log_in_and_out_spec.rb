47decd46146b54c9c27860972a3441280374714f
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-07 15:29:28 -0600

    created admin login feature

---
+++ spec/features/admin/admin_user_can_log_in_and_out_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "Admin user can log in and out" do
+  scenario "if accout exists" do
+    admin = User.create(username: "AdminDude", password: "abc", role: "admin")
+
+    visit '/'
+    click_on("Login")
+
+    expect(current_path).to eq(login_path)
+
+    fill_in 'session[username]', with: admin.username
+    fill_in 'session[password]', with: admin.password
+
+    click_on("Login")
+
+    expect(page).to have_content("Login Successful")
+    expect(page).to have_content("Hi #{admin.username}")
+
+    expect(page).to have_link("Logout")
+    click_on "Logout"
+    expect(current_path).to eq(root_path)
+  end
+end
