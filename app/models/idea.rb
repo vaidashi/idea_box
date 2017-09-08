112a10b0c14b9c6ffe9dbf6ac02440670bc2a750
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-08 14:01:00 -0600

    modified model to reflect new db relationships

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -5,4 +5,7 @@ class Idea < ApplicationRecord
 
   belongs_to :category
   belongs_to :user
+
+  has_many :idea_images
+  has_many :images, through: :idea_images
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,7 @@
 class Image < ApplicationRecord
+  validates :name, presence: true
+  validates :image_path, presence: true
+
+  has_many :idea_images
+  has_many :ideas, through: :idea_images
 end
