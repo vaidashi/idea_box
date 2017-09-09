826272eb5a17f1dbe8ba13b20258aa9047f58c5b
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-09 13:56:53 -0600

    create image feature added

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -2,7 +2,27 @@
 class Admin::ImagesController < Admin::BaseController
 
   def index
-    @images = Image.all 
+    @images = Image.all
   end
 
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      flash[:notice] = "You added a new image"
+      redirect_to admin_images_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def image_params
+    params.require(:image).permit(:name, :image_path)
+  end
 end
